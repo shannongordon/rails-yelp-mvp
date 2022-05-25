5c2966b0aec4e2817722672ecbad06d80e1f9d65
Shannon Gordon <shazkroeger@gmail.com>
2022-05-25 12:45:40 +1000

    restaurant rating

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+restaurants = Restaurant.create([{ name: 'Bam Bam', address: 'Avonsleigh', category: 'italian'}, { name: 'Cafe 57', address: 'Notting Hill', category: 'french'}, { name: 'General Store', address: 'Emerald', category: 'italian'}, { name: 'Omi', address: 'Glen Waverley', category: 'japanese'}, { name: 'Cheiftans', address: 'Cockatoo', category: 'belgian'}])
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
